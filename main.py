2fb013223dc0b327fd8ce4a05c4a047dab574350
CyberTomB <tomcbowers@gmail.com>
2024-12-05 14:32:25 -0700

    add delta time

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import pygame
 from constants import *
 
 def main():
-    # pygame.init()
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    global dt
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -14,6 +16,7 @@ def main():
                 return
         screen.fill(color=(0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
