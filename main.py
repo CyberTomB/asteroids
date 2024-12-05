a1e0ef12844b47e242199baa4a1809582a9fbb90
CyberTomB <tomcbowers@gmail.com>
2024-12-05 14:26:12 -0700

    establish working pygame window!

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+import os
+os.environ['SDL_AUDIODRIVER'] = 'dsp'
 import pygame
+from constants import *
 
 def main():
-    print("Starting asteroids!")
+    # pygame.init()
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(color=(0, 0, 0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
